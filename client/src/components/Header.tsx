5052a1b7efe881169cd57fd28f9c388a9d39b766
Miloepeng <yeoyongsheng2003@gmail.com>
2025-05-21 12:09:06 +0800

    please make it a folder not a repo

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="headerContainer">
+      <div className="headerCenter">
+        <h1 className="headerTitle">CAP Calculator</h1>
+        {/* Placeholder for future nav or login */}
+        <div className="navBar">navBar test change</div>
+      </div>
+    </header>
+  );
+}
---
+++ client/src/types.ts
@@ -0,0 +1,7 @@
+export interface Module {
+    id: string;
+    name: string;
+    grade: string;
+    su: boolean;
+    is2MC: boolean;
+}
