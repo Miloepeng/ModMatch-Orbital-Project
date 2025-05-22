2b98103a8a7dfc2a68c63bfdb92ac4e8fc5e7a33
Robin <Lvbrobinhood@gmail.com>
2025-05-22 19:24:44 +0800

    2nd Attempt to push by Robin

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,12 +3,23 @@ import logo from "../assets/Logo1.png";
 
 export default function Header() {
   return (
-    <header className="headerContainer">
+    /*<header className="headerContainer">
       <div className="headerCenter">
         <img src = {logo} />
         <h1 className="headerTitle">CAP Calculator</h1>
-        {/* Placeholder for future nav or login */}
+        {/* Placeholder for future nav or login }
         <div className="navBar">navBar test change</div>
+      </div>
+    </header>*/
+
+    <header className="headerContainer">
+      <div className = "headerCenter">
+        <img src = {logo} width="50" />
+        <div className="box">Home</div>
+        <div className="box">Search</div>
+        <div className="box">Degree Requirement</div>
+        <div className="box">Timetable</div>
+        <div className="box"></div>
       </div>
     </header>
   );
