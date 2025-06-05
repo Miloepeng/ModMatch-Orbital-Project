85c4785da4ea3c01f4b031a29e0d7bfc1bca5f57
Miloepeng <yeoyongsheng2003@gmail.com>
2025-06-05 14:37:44 +0800

    minor css changes to filters

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { modules } from '../utils/modules'
+import { modules } from '../utils/modules';
+import './Filter.css';
 
 export default function Filter() {
     const [searchQuery, setSearchQuery] = useState("");
@@ -8,7 +9,7 @@ export default function Filter() {
     const [SUFilter, setSUFilter] = useState("all");
 
     const filteredModules = modules.filter((module) => {
-        const matchesSearch = module.label.toUpperCase().includes(searchQuery);
+        const matchesSearch = module.label.toUpperCase().includes(searchQuery.toUpperCase());
         const matchesExam = examFilter === 'all' || module.exam === examFilter;
         
         return matchesSearch && matchesExam;
@@ -17,25 +18,27 @@ export default function Filter() {
     return (
         <>
         <div>
+            <div>
             <input
+            className = "search-box"
             type="text"
             placeholder='Search...'
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             />
-
-            <select value={examFilter} onChange = {(e) => setExamFilter(e.target.value)}>
+            </div>
+            <select value={examFilter} onChange = {(e) => setExamFilter(e.target.value)} className="filter-box">
                 <option value='all'>All</option>
                 <option value='mcq'>MCQ</option>
                 <option value='openended'>Open-Ended</option>
                 <option value='mixed'>Mixed</option>
             </select>
 
-            <ul>
+            <ol className="modules-list">
                 {filteredModules.map((module) => (
                     <li>{module.value} - {module.exam}</li>
                 ))}
-            </ul>
+            </ol>
         </div>
         </>
     )
